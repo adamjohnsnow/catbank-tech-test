7b5944a3dea445df67a6bafd50481f7461311ec8
Adam Snow <adamjohnsnow@icloud.com>
2023-11-15 13:12:59 +0000

    db seeds and inital screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,29 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { seed } from "@/lib/seed";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import seedTables from "@/components/table";
 
 export const metadata = {
-  metadataBase: new URL('https://postgres-starter.vercel.app'),
-  title: 'Vercel Postgres Demo',
-  description:
-    'A simple Next.js app with Vercel Postgres as the database',
-}
+  metadataBase: new URL("https://postgres-starter.vercel.app"),
+  title: "Vercel Postgres Demo",
+  description: "A simple Next.js app with Vercel Postgres as the database",
+};
 
 const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-  display: 'swap',
-})
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  seedTables();
   return (
     <html lang="en">
       <body className={inter.variable}>{children}</body>
     </html>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   images: {
     domains: ["images.ctfassets.net"],
-  },
+  },experimental: {
+    serverActions: true,
+    },
 };
 
 module.exports = nextConfig;
